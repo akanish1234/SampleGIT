192ee34b36bd36ffe46fd7c085d86e06eeef8c08
akanish1234 <akanish2208@gmail.com>
2024-09-14 09:51:35 +0530

    new project added

--- src/main/java/org/sample/Sample2.java
+++ src/main/java/org/sample/Sample2.java
@@ -1,5 +1,23 @@
 package org.sample;
 
 public class Sample2 {
+	
+	public void testing() {
+		System.out.println("Automation");
 
+	}
+	public void Program() {
+		System.out.println("Java");
+
+	}
+	public void BAT() {
+		System.out.println("Verification");
+	}
+
+	public static void main(String[] args) {
+		Sample2 s2 = new Sample2();
+		s2.BAT();
+		s2.testing();
+		s2.Program();
+	}
 }
